f180467f87ae37321a0ea095702867bc9838f249
Mohammad Laraib <mohammadlaraib34@gmail.com>
2024-03-02 17:41:26 +0530

    Navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,29 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 import { FaGraduationCap } from "react-icons/fa6";
-import { useState } from 'react'
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import '../Styles/Navbar.css'
-import '../App.css'
+import "../Styles/Navbar.css";
+import "../App.css";
 
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
 
   const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar)
-  }
+    setShowNavbar(!showNavbar);
+  };
 
   return (
     <nav className="navbar">
       <div className="container">
         <div className="logo">
-          <FaGraduationCap size={40} color='#114232' />
+          <FaGraduationCap size={40} color="#114232" />
         </div>
+
         <div className="menu-icon" onClick={handleShowNavbar}>
-          <GiHamburgerMenu size={30} color='#114232'/>
+          <GiHamburgerMenu size={30} color="#114232" />
         </div>
-        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
           <ul>
             <li>
               <NavLink to="/">Home</NavLink>
@@ -35,14 +37,18 @@ const Navbar = () => {
             <li>
               <NavLink to="/about">About</NavLink>
             </li>
+          </ul>
+        </div>
+        <div className="signup nav-elements ">
+          <ul>
             <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink to="/signup">Sign Up</NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
